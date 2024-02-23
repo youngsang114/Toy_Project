69fe5ed5721c7338277a9b53a3053ce11f37400e
yys <dgh06008@naver.com>
2024-02-23 22:12:21 +0900

    24.02.23

--- loginBoard/src/main/java/hello/loginBoard/WebConfig.java
+++ loginBoard/src/main/java/hello/loginBoard/WebConfig.java
@@ -1,9 +1,10 @@
 package hello.loginBoard;
 
 import hello.loginBoard.member.interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {

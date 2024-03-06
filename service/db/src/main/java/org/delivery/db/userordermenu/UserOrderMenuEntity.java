519dab12d3554960d109fe0a97034a0805be4d1f
yys <dgh06008@naver.com>
2024-03-06 23:25:29 +0900

    Feat : UserOrderMenuEntity 추가

---
+++ service/db/src/main/java/org/delivery/db/userordermenu/UserOrderMenuEntity.java
@@ -0,0 +1,34 @@
+package org.delivery.db.userordermenu;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.delivery.db.BaseEntity;
+import org.delivery.db.storemenu.StoreMenuEntity;
+import org.delivery.db.userorder.UserOrderEntity;
+import org.delivery.db.userordermenu.enums.UserOrderMenuStatus;
+
+@Entity
+@Table(name = "user_order_menu")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+public class UserOrderMenuEntity extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_order_id")
+    private UserOrderEntity userOrder; // UserOrderMenu : UserOrder = n : 1
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_menu_id")
+    private StoreMenuEntity storeMenu;  // UserOrderMenu : StoreMenu = n : 1
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 50, nullable = false)
+    private UserOrderMenuStatus status;
+}
---
+++ service/db/src/main/java/org/delivery/db/userordermenu/enums/UserOrderMenuStatus.java
@@ -0,0 +1,12 @@
+package org.delivery.db.userordermenu.enums;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum UserOrderMenuStatus {
+
+    REGISTERED("등록"),
+    UNREGISTERED("해지");
+
+    private String description;
+}

be0838bdbaa91d999eb4ffbd3bdf1a2d9c8a9443
yys <dgh06008@naver.com>
2024-03-11 08:36:44 +0900

    Feat: ItemRepository 추가

---
+++ shop/src/main/java/com/shop/item/repository/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.shop.item.repository;
+
+import com.shop.item.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item,Long> {
+}

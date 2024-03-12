18e954ace6da2b759392fe5a6929ff31fb3bf8b7
yys <dgh06008@naver.com>
2024-03-12 19:51:50 +0900

    Feat: Errorcode추가

---
+++ blog/src/main/java/com/example/blog/common/error/ErrorCode.java
@@ -0,0 +1,18 @@
+package com.example.blog.common.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public enum ErrorCode implements ErrorCodeIfs{
+    OK(200,200,"성공"),
+    BAD_REQUEST(400,400,"잘못된 요청"),
+    SERVER_ERROR(500,500,"서버 에러"),
+    NULL_POINT(HttpStatus.INTERNAL_SERVER_ERROR.value(),512,"NullPointError");
+
+    private final Integer httpStatusCode;
+    private final Integer errorCode;
+    private final String description;
+}

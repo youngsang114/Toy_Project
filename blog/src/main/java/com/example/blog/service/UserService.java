adfcf20b08fafe5ec275e704673d054e22d2b6ad
yys <dgh06008@naver.com>
2024-03-16 17:10:16 +0900

    Feat : User Service 추가

---
+++ blog/src/main/java/com/example/blog/service/UserService.java
@@ -0,0 +1,24 @@
+package com.example.blog.service;
+
+import com.example.blog.common.error.UserError;
+import com.example.blog.common.exception.ApiException;
+import com.example.blog.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new ApiException(UserError.NOT_FOUND_USER));
+    }
+}

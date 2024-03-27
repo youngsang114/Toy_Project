d3d5a560b69f3c8787b852869e83f32fc83a5a89
yys <dgh06008@naver.com>
2024-03-27 19:34:02 +0900

    24.03.27

--- service/store-admin/src/main/java/org/delivery/storeadmin/config/security/SecurityConfig.java
+++ service/store-admin/src/main/java/org/delivery/storeadmin/config/security/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.List;
@@ -24,27 +26,36 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .authorizeHttpRequests(it -> {
+                .authorizeHttpRequests(it ->{
                     it
-                            // resource 에 대해서는 모든 요청 허용
                             .requestMatchers(
                                     PathRequest.toStaticResources().atCommonLocations()
-                            ).permitAll()
-                            // open-api /  하위 모든 주소는 인증 없이 통과
-                            .requestMatchers(
-                                    "/open-api/**"
-                            ).permitAll()
-                            // 스웨거는 인증 없이 통과
+                            ).permitAll()   // resource 에 대해서는 모든 요청 허용
+
+                            // swagger 는 인증 없이 통과
                             .requestMatchers(
                                     SWAGGER.toArray(new String[0])
                             ).permitAll()
 
-                            // 그외 모든 요청은 인증 사용
-                            .anyRequest().authenticated();
+                            // open-api / ** 하위 모든 주소는 인증 없이 통과
+                            .requestMatchers(
+                                    "/open-api/**"
+                            ).permitAll()
+
+                            // 그 외 모든 요청은 인증 사용
+                            .anyRequest().authenticated()
+                    ;
                 })
                 .formLogin(Customizer.withDefaults())
         ;
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+
+        // hash 방식으로 암호화
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ service/store-admin/src/main/java/org/delivery/storeadmin/domain/user/controller/model/StoreUserResponse.java
@@ -0,0 +1,54 @@
+package org.delivery.storeadmin.domain.user.controller.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.catalina.Store;
+import org.apache.catalina.User;
+import org.apache.el.lang.ELArithmetic;
+import org.delivery.db.storeuser.enums.StoreUserRole;
+import org.delivery.db.storeuser.enums.StoreUserStatus;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StoreUserResponse {
+
+    private UserResponse user;
+    private StoreResponse store;
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class UserResponse{
+
+        private Long id;
+
+        private String email;
+
+        private StoreUserStatus status;
+
+        private StoreUserRole role;
+
+        private LocalDateTime registeredAt;
+        private LocalDateTime unregisteredAt;
+        private LocalDateTime lastLoginAt;
+
+    }
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class StoreResponse{
+        private Long id;
+        private String name;
+
+    }
+}

f9568c4cbe6239a5333ff0f157cdc5c69fa6ef0e
yys <dgh06008@naver.com>
2024-05-21 21:16:07 +0900

    Object Mapper

--- rest-api/src/main/java/com/example/restapi/model/UserRequest.java
+++ rest-api/src/main/java/com/example/restapi/model/UserRequest.java
@@ -1,5 +1,7 @@
 package com.example.restapi.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
@@ -13,9 +15,14 @@ import lombok.NoArgsConstructor;
 public class UserRequest {
     private String userName;
     private Integer userAge;
+    @JsonProperty("user_email")
     private String email;
     private Boolean isKorean; // is_korean, boolean의 기본값 false
                               // primitive type에서 is 시리지는 boolean을 듯하기 때문에 setIsKorean이 아닌 SetKorean으로 만들어진다
                               // 1. 요청시 korean으로 요청한다(json에 전송하는 의미를 전달 못한다)
                               // 2. wrapper 클래스인 Boolean으로 고치자
+    @JsonIgnore
+    public String getUser(){
+        return userName;
+    }
 }
--- rest-api/src/test/java/com/example/restapi/RestApiApplicationTests.java
+++ rest-api/src/test/java/com/example/restapi/RestApiApplicationTests.java
@@ -1,13 +1,33 @@
 package com.example.restapi;
 
+import com.example.restapi.model.UserRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RestApiApplicationTests {
 
+	@Autowired // 스프링에서 관리하는 빈들 중에서 자동으로 생성되는 오브젝트 메퍼를 가져오겠다는 의미
+	private ObjectMapper objectMapper;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws JsonProcessingException {
+		UserRequest user = new UserRequest();
+
+		user.setUserName("홍길동");
+		user.setUserAge(10);
+		user.setEmail("hong@hmail.com");
+		user.setIsKorean(true);
+
+		String json = objectMapper.writeValueAsString(user);// 직렬화를 쓰기 위해서 write를 한다
+		System.out.println(json);  // {"user_name":"홍길동","user_age":10,"email":"hong@hmail.com","is_korean":true}
+
+		UserRequest dto = objectMapper.readValue(json, UserRequest.class);
+		System.out.println(dto); // UserRequest(userName=홍길동, userAge=10, email=hong@hmail.com, isKorean=true)
+
 	}
 
 }

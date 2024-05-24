8d18c55c6594ee94ebc89f9d90a2f68fe2b6e5b4
yys <dgh06008@naver.com>
2024-05-24 23:09:15 +0900

    Validation

--- fast/validation/src/main/java/com/example/validation/model/UserRegisterRequest.java
+++ fast/validation/src/main/java/com/example/validation/model/UserRegisterRequest.java
@@ -1,5 +1,6 @@
 package com.example.validation.model;
 
+import annotation.PhoneNumber;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.*;
@@ -9,6 +10,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Data
 @AllArgsConstructor
@@ -19,8 +21,10 @@ public class UserRegisterRequest {
 
 //    @NotNull  // != null
 //    @NotEmpty // != null && name != ""
-    @NotBlank // != null && name != "" && name != "   "
+//    @NotBlank // != null && name != "" && name != "   "
     private String name;
+
+    private String nickName;
 
     @NotBlank
     @Size(min = 1, max = 12)
@@ -34,9 +38,20 @@ public class UserRegisterRequest {
     @Email
     private String email;
 
-    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "휴대폰 번호 양식과 맞지 않습니다")
+    @PhoneNumber
+//    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "휴대폰 번호 양식과 맞지 않습니다")
     private String phoneNumber;
 
     @FutureOrPresent // 현재 or 미래
     private LocalDateTime registerAt;
+    @AssertTrue(message = "name or nickName 중 반듯이 1개가 존재해야 합니다") // 해당 리턴값이 true일 때 실행하는 어노테이션, 반듯이 is라는 메서드에 붙여줘야 한다
+    public boolean isNameCheck(){
+        if (Objects.nonNull(name) && !name.isBlank()){
+            return true;
+        }
+        if (Objects.nonNull(nickName) && !nickName.isBlank()){
+            return true;
+        }
+        return false;
+    }
 }

bb4bde67a9ebd439390f11cbfc0347bc2fddbc7a
yys <dgh06008@naver.com>
2024-06-29 14:53:52 +0900

    Feat: front와 오류없이 소통하기 위한 cors 설정

--- SecurityJWT/src/main/java/com/study/SecurityJWT/config/SecurityConfig.java
+++ SecurityJWT/src/main/java/com/study/SecurityJWT/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.study.SecurityJWT.config;
 import com.study.SecurityJWT.jwt.JWTFilter;
 import com.study.SecurityJWT.jwt.JWTUtil;
 import com.study.SecurityJWT.jwt.LoginFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -35,6 +40,24 @@ public class SecurityConfig {
 
 
         httpSecurity
+                .cors((corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration configuration = new CorsConfiguration();
+
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L);
+
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                        return configuration;
+                    }
+                })))
                 .csrf((auth) -> auth.disable())     // csrf disable(Session 방식에서는 필요, JWT 방식에서는 필요 없음)
                 .formLogin((auth) -> auth.disable()) // from 로그인 방식 disable
                 .httpBasic((auth) -> auth.disable()) // http basic 인증 방식 disable

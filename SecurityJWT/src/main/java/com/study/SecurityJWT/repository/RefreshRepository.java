9f07a6aeb924428d195911992f55d7972c4b5255
yys <dgh06008@naver.com>
2024-06-30 15:33:35 +0900

    Feat: refresh token의 entity,repository 생성

---
+++ SecurityJWT/src/main/java/com/study/SecurityJWT/repository/RefreshRepository.java
@@ -0,0 +1,14 @@
+package com.study.SecurityJWT.repository;
+
+import com.study.SecurityJWT.entity.RefreshEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface RefreshRepository extends JpaRepository<RefreshEntity,Long> {
+
+    Boolean existedByRefresh(String refresh);
+
+    // 과거의 refresh token을 삭제하기 위한 메서드
+    @Transactional
+    void deleteByRefresh(String refresh);
+}
